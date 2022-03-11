9aded8d6166e7ea20d73576d1dcc02ee998ee0f8
zlimaz <98031566+zlimaz@users.noreply.github.com>
2022-03-11 17:04:34 -0300

    Update fatorial.py

--- fatorial.py
+++ fatorial.py
@@ -5,11 +5,13 @@ Crie um programa que peça um número e calcule o seu fatorial
 * for: 1
 """
 
-fat *= x
-fat *= x + 1
-fat = 0
+n = int(input("n: "))
 fat = 1
-for i in range(n):
-    for x in range(1, n + 1):
-        n = int(input("n: "))
-        print(f"fat({n}) = {fat}")
+for x in range(1, n + 1):
+    fat *= x
+
+print(f"fat({n}) = {fat}")
+
+#fat *= x + 1
+#fat = 0
+#for i in range(n):
