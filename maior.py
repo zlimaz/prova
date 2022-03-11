a0f7505a12d10a82af88aa8cd251c3d74141418e
zlimaz <98031566+zlimaz@users.noreply.github.com>
2022-03-11 16:57:36 -0300

    Update maior.py

--- maior.py
+++ maior.py
@@ -7,16 +7,19 @@ elemento.
 * if: 1
 """
 
+
+ns = [int(x) for x in input("ns: ").split(",")]
+
+
 def maxarg(lst):
+    max_pos = 0
+    max_value = lst[0]
     for i, x in enumerate(lst):
+        if x > max_value:
+            max_value = x
+            max_pos = i
         if not lst:
-            if x > max_value:
-                max_pos = 0
-                max_pos = i
-                max_value = lst[0]
-                max_value = x
-                ns = [int(x) for x in input("ns: ").split(",")]
-                pos, valor = maxarg(ns)
-                print(f"maior valor = {valor}, posicao = {pos + 1}o")
-                return (max_pos, max_value)
-                return None
+            return None
+    return (max_pos, max_value)
+pos, valor = maxarg(ns)
+print(f"maior valor = {valor}, posição = {pos + 1}o")
